27bf103094b71b7e77f16d5800d4b1089fb4be9d
Jon Allemand <jon_allemand@live.com>
2025-11-28 08:36:34 +0100

    chore: update cli arg names

--- src/f2s3/cli.py
+++ src/f2s3/cli.py
@@ -10,12 +10,12 @@ from f2s3.config import F2S3Config
 def process():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-s', '--source_cloud', type=Path,
+        '-s', '--source', type=Path,
         help='Path to the source point cloud (ignored if ``--start_from_tiled_data``).'
     )
 
     parser.add_argument(
-        '-t', '--target_cloud', type=Path,
+        '-t', '--target', type=Path,
         help='Path to the target point cloud (ignored if ``--start_from_tiled_data``).'
     )
 
@@ -31,7 +31,7 @@ def process():
     )
 
     parser.add_argument(
-        '-r', '--results_dir', type=str, default="",
+        '-r', '--output_folder', type=str, default="",
         help='Path to the root results directory'
     )
 
@@ -60,6 +60,15 @@ def process():
         help='Maximum displacement magnitude, points with higher magnitude will be filtered out (no filtering if 0)'
     )
 
+    parser.add_argument(
+        '--minimum_points', type=int, default=argparse.SUPPRESS,
+        help='Number of points used to compute normal vectors in the supervoxels'
+    )
+
+    parser.add_argument(
+        '--n_normals', type=int, default=argparse.SUPPRESS,
+        help='Number of points used to compute normal vectors in the supervoxels'
+    )
 
     parser.add_argument(
         '--save_interim', action='store_true',
@@ -106,19 +115,8 @@ def process():
         help='Multiplier for the median magnitude filter'
         )
 
-    parser.add_argument(
-        '--n_normals', type=int, default=argparse.SUPPRESS,
-        help='Number of points used to compute normal vectors in the supervoxels'
-        )
-
-    parser.add_argument(
-        '--minimum_points', type=int, default=argparse.SUPPRESS,
-        help='Number of points used to compute normal vectors in the supervoxels'
-        )
-
     args = parser.parse_args()
 
-    # TODO update this list to reflect the config file
     run_settings = F2S3Config(**vars(args))
 
     with torch.no_grad():
